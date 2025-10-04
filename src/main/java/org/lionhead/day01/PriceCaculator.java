1fa406addcc625c97f7e16239a576423513d5fe3
lionhead <657116847@qq.com>
2025-10-04 23:59:38 +0800

    JavaUnitTest:单元测试范围: pricePlus & priceSum

--- src/main/java/org/lionhead/day01/PriceCaculator.java
+++ src/main/java/org/lionhead/day01/PriceCaculator.java
@@ -1,7 +1,28 @@
 package org.lionhead.day01;
 
 public class PriceCaculator {
-    public static int calPriceSum(int priceA, int priceB) {
+    /**
+     * 计算价格的总和 + 快递费
+     * @param priceA 价格 A
+     * @param priceB 价格 B
+     * @return 总和 + 快递费
+     */
+    public int calTotalPricePlus(int priceA, int priceB) {
+        int total = calPriceSum(priceA, priceB);
+        if (total < 100) {
+            return total + 20;
+        } else {
+            return total;
+        }
+    }
+
+    /**
+     * 计算价格的总和
+     * @param priceA 价格 A
+     * @param priceB 价格 B
+     * @return 两个价格的总和
+     */
+    public int calPriceSum(int priceA, int priceB) {
         return priceA + priceB;
     }
 }
--- src/main/java/org/lionhead/day01/PriceCalTester.java
+++ src/main/java/org/lionhead/day01/PriceCalTester.java
@@ -8,18 +8,32 @@ public class PriceCalTester {
         // 条件 -> case
         // - 正常
         // 调用一个两数之和的函数，返回一个结果
-        return case1Test(priceCaculator) && case2Test(priceCaculator) && case3Test(priceCaculator);
+        boolean priceTestResult = case1Test(priceCaculator) && case2Test(priceCaculator) && case3Test(priceCaculator);
+        boolean pricePlusTestResult =  pricePlusTest1(priceCaculator) && pricePlusTest2(priceCaculator) && pricePlusTest3(priceCaculator);
+
+        return priceTestResult && pricePlusTestResult;
     }
 
-    public  boolean case1Test(PriceCaculator priceCaculator) {
-        return priceCaculator.calPriceSum(1, 2) == 3;
+    public  boolean pricePlusTest1(PriceCaculator priceCaculator) {
+        return priceCaculator.calTotalPricePlus(10, 20) == 50;
     }
 
-    public  boolean case2Test(PriceCaculator priceCaculator) {
-        return priceCaculator.calPriceSum(2, 5) == 7;
+    public  boolean pricePlusTest2(PriceCaculator priceCaculator) {
+        return priceCaculator.calTotalPricePlus(2, 5) == 27;
     }
 
-    public  boolean case3Test(PriceCaculator priceCaculator) {
+    public  boolean pricePlusTest3(PriceCaculator priceCaculator) {
+        return priceCaculator.calTotalPricePlus(50, 60) == 110;
+    }
+
+
+    private static boolean case1Test(PriceCaculator priceCaculator){
+        return priceCaculator.calPriceSum(1,2) == 3;
+    }
+    private static boolean case2Test(PriceCaculator priceCaculator){
+        return priceCaculator.calPriceSum(2,5) == 7;
+    }
+    private static boolean case3Test(PriceCaculator priceCaculator){
         return priceCaculator.calPriceSum(-1, 5) == 4;
     }
 }
