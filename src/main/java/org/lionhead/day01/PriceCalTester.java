5e8a35173300fa40f524500dfa1705cc3fa0f4e9
lionhead <657116847@qq.com>
2025-10-05 16:59:46 +0800

    JavaUnitTest:替换的艺术:引入断言assert

--- src/main/java/org/lionhead/day01/PriceCalTester.java
+++ src/main/java/org/lionhead/day01/PriceCalTester.java
@@ -6,58 +6,60 @@ public class PriceCalTester {
     private final PriceCalculator priceCalculator = new PriceCalculator();
 
     // 单元测试函数/方法
-    public  boolean testMethodEntry() {
+    public void testMethodEntry() {
         // 条件 -> case
         // - 正常
         // 调用一个两数之和的函数，返回一个结果
-        boolean priceTestResult = case1Test(priceCalculator)
-                && case2Test(priceCalculator)
-                && case3Test(priceCalculator);
-        boolean pricePlusTestResult =  pricePlusTest1(priceCalculator)
-                && pricePlusTest3(priceCalculator);
-        boolean newResult = ge100le150(priceCalculator)
-                && ge150le200(priceCalculator)
-                && le30(priceCalculator);
-        boolean unitTestResult = unitTestCalTotalPricePlus(priceCalculator);
+        case1Test(priceCalculator);
+        case2Test(priceCalculator);
+        case3Test(priceCalculator);
 
-        return priceTestResult && pricePlusTestResult && newResult && unitTestResult;
+        pricePlusTest1(priceCalculator);
+        pricePlusTest3(priceCalculator);
+
+        ge100le150(priceCalculator);
+        ge150le200(priceCalculator);
+        le30(priceCalculator);
+
+        unitTestCalTotalPricePlus(priceCalculator);
+
     }
 
-    public boolean unitTestCalTotalPricePlus(PriceCalculator priceCalculator) {
+    public void unitTestCalTotalPricePlus(PriceCalculator priceCalculator) {
         PriceCalculator priceCalculator1 = new PriceCalculatorDouble(90);
-        ExpressServiceDouble expressServiceDouble = new ExpressServiceDouble(20,1,5);
+        ExpressServiceDouble expressServiceDouble = new ExpressServiceDouble(20,20,20);
         priceCalculator1.expressService = expressServiceDouble;
         expressServiceDouble.spy = new HashMap<>();
-        return priceCalculator1.calTotalPricePlus(5, 85) == 110 && expressServiceDouble.spy.get("queryExpressPrice") == 1;
+        assert  priceCalculator1.calTotalPricePlus(5, 85) == 110 && expressServiceDouble.spy.get("queryExpressPrice") == 1;
     }
 
 
-    public  boolean pricePlusTest1(PriceCalculator priceCalculator) {
-        return priceCalculator.calTotalPricePlus(10, 20) == 50;
+    public void pricePlusTest1(PriceCalculator priceCalculator) {
+        assert priceCalculator.calTotalPricePlus(10, 20) == 50;
     }
-    public  boolean pricePlusTest3(PriceCalculator priceCalculator) {
-        return priceCalculator.calTotalPricePlus(50, 60) == 120;
-    }
-
-
-    public boolean le30(PriceCalculator priceCalculator) {
-        return priceCalculator.calTotalPricePlus(5,10) == 40;
-    }
-    public boolean ge100le150(PriceCalculator priceCalculator) {
-        return priceCalculator.calTotalPricePlus(80,30) == 120;
-    }
-    public boolean ge150le200(PriceCalculator priceCalculator) {
-        return priceCalculator.calTotalPricePlus(100, 70) == 175;
+    public void pricePlusTest3(PriceCalculator priceCalculator) {
+        assert priceCalculator.calTotalPricePlus(50, 60) == 120;
     }
 
 
-    private static boolean case1Test(PriceCalculator priceCalculator){
-        return priceCalculator.calPriceSum(1,2) == 3;
+    public void le30(PriceCalculator priceCalculator) {
+        assert priceCalculator.calTotalPricePlus(5,10) == 40;
     }
-    private static boolean case2Test(PriceCalculator priceCalculator){
-        return priceCalculator.calPriceSum(2,5) == 7;
+    public void ge100le150(PriceCalculator priceCalculator) {
+        assert priceCalculator.calTotalPricePlus(80,30) == 120;
     }
-    private static boolean case3Test(PriceCalculator priceCalculator){
-        return priceCalculator.calPriceSum(-1, 5) == 4;
+    public void ge150le200(PriceCalculator priceCalculator) {
+        assert priceCalculator.calTotalPricePlus(100, 70) == 175;
+    }
+
+
+    public void case1Test(PriceCalculator priceCalculator){
+        assert priceCalculator.calPriceSum(1,2) == 3;
+    }
+    public void case2Test(PriceCalculator priceCalculator){
+        assert priceCalculator.calPriceSum(2,5) == 7;
+    }
+    public void case3Test(PriceCalculator priceCalculator){
+        assert priceCalculator.calPriceSum(-1, 5) == 4;
     }
 }
--- src/main/java/org/lionhead/day01/ValueOfUnitTest.java
+++ src/main/java/org/lionhead/day01/ValueOfUnitTest.java
@@ -4,13 +4,13 @@ public class ValueOfUnitTest {
     // 主入口
     public static void main(String[] args) {
         PriceCalTester priceCalTester = new PriceCalTester();
-        boolean testResult = priceCalTester.testMethodEntry();
+        priceCalTester.testMethodEntry();
 
-        if (testResult){
-            startAllSystem();
-        } else {
-            System.out.println("WARNING!!!ERROR");
-        }
+//        if (testResult){
+//            startAllSystem();
+//        } else {
+//            System.out.println("WARNING!!!ERROR");
+//        }
 
     }
 
